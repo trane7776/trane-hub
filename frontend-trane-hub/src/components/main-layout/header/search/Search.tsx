a69b1663df60cb7dc7752dd252f3e1e3478845ce
trane7776 <izbasarov.t@edu.rea.ru>
2024-12-29 22:15:42 +0300

    updated

--- frontend-trane-hub/src/components/main-layout/header/search/Search.tsx
+++ frontend-trane-hub/src/components/main-layout/header/search/Search.tsx
@@ -1,9 +1,12 @@
+import { cn } from '@/lib/utils';
 import React from 'react';
+
+import styles from './Search.module.scss';
 
 interface Props {
     className?: string;
 }
 
 export const Search: React.FC<Props> = ({ className }) => {
-    return <div className={className}>Search</div>;
+    return <div className={cn(styles.search)}>Search</div>;
 };
--- frontend-trane-hub/src/components/main-layout/header/search/useSearch.ts
+++ frontend-trane-hub/src/components/main-layout/header/search/useSearch.ts
@@ -1,25 +1,26 @@
-import { useDebounce } from "@/hooks/useDebounce";
-import { movieService } from "@/services/movie.service";
-import { useQuery } from "@tanstack/react-query";
-import { ChangeEvent, useState } from "react"
+import { useDebounce } from '@/hooks/useDebounce';
+import { movieService } from '@/services/movie.service';
+import { useQuery } from '@tanstack/react-query';
+import { ChangeEvent, useState } from 'react';
 
 export const useSearch = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const debouncedSearch = useDebounce(searchTerm, 500);
 
-    const {isSuccess, data} = useQuery({
+    const { isSuccess, data } = useQuery({
         queryKey: ['search movies', debouncedSearch],
         queryFn: () => movieService.getAll(debouncedSearch),
-        enabled: !!debouncedSearch
+        enabled: !!debouncedSearch,
     });
 
     const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
-        setSearchTerm(event.target.value)
-    }
-
+        setSearchTerm(event.target.value);
+    };
 
     return {
         isSuccess,
-        handleSearch,data,searchTerm
-    }
-}
+        handleSearch,
+        data,
+        searchTerm,
+    };
+};
