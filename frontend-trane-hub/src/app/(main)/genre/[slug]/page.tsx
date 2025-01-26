6c6166b12eb72b52a342ac2efa0a25cfec8b1c72
trane7776 <izbasarov.t@edu.rea.ru>
2025-01-26 21:58:21 +0300

    feat:added ssg genre pages

--- frontend-trane-hub/src/app/(main)/genre/[slug]/page.tsx
+++ frontend-trane-hub/src/app/(main)/genre/[slug]/page.tsx
@@ -1,10 +1,66 @@
-import { IPageSlugParam } from '@/types/page-params.types';
+import { Catalog } from '@/components/ui/catalog-movies/Catalog';
+import { genreService } from '@/services/genre.service';
+import { movieService } from '@/services/movie.service';
+import { IPageSlugParam, TypeParamSlug } from '@/types/page-params.types';
 import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 
-export const metadata: Metadata = {
-    title: 'Жанр',
-};
+export const revalidate = 60;
 
-export default function GenrePage({ params }: IPageSlugParam) {
-    return <div>{params.slug}</div>;
+export async function generateStaticParams() {
+    const genres = await genreService.getAll();
+
+    const paths = genres.map((genre) => {
+        return {
+            params: {
+                slug: genre.slug,
+            },
+        };
+    });
+
+    return paths;
+}
+
+async function getMovies(params: TypeParamSlug) {
+    try {
+        const { data: genre } = await genreService.getBySlug(params.slug);
+        if (!genre) return redirect('/404');
+
+        const { data: movies } = await movieService.getByGenres([genre.id]);
+        return { genre, movies };
+    } catch (error) {
+        return redirect('/404');
+    }
+}
+
+export async function generateMetadata({
+    params,
+}: IPageSlugParam): Promise<Metadata> {
+    const { genre, movies } = await getMovies(params);
+
+    return {
+        title: genre.name,
+        description: genre.description,
+        openGraph: {
+            images: {
+                url: movies[0]?.poster,
+            },
+
+            description: genre.description,
+        },
+    };
+}
+
+export default async function GenrePage({ params }: IPageSlugParam) {
+    const { genre, movies } = await getMovies(params);
+
+    return (
+        <div className="px-6">
+            <Catalog
+                title={genre.name}
+                description={genre.description}
+                movies={movies}
+            />
+        </div>
+    );
 }
