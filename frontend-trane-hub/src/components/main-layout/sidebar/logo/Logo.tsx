60af62053c1147d4697ece9de3277f7967ac73a9
trane7776 <izbasarov.t@edu.rea.ru>
2025-04-28 02:45:39 +0300

    fix

--- frontend-trane-hub/src/components/main-layout/sidebar/logo/Logo.tsx
+++ frontend-trane-hub/src/components/main-layout/sidebar/logo/Logo.tsx
@@ -3,18 +3,12 @@ import React from 'react';
 
 import styles from './Logo.module.scss';
 import Link from 'next/link';
-import { Poppins } from 'next/font/google';
 import { PUBLIC_URL } from '@/config/url.config';
 import { cn } from '@/lib/utils';
 import { HandySvg } from 'handy-svg';
 interface Props {
     className?: string;
 }
-
-const font = Poppins({
-    subsets: ['latin'],
-    weight: ['600'],
-});
 
 export const Logo: React.FC<Props> = ({ className }) => {
     return (
@@ -27,8 +21,7 @@ export const Logo: React.FC<Props> = ({ className }) => {
             />
             <div
                 className={cn(
-                    'font-semibold text-2xl text-white',
-                    font.className
+                    'font-semibold text-2xl text-white'
                 )}
             >
                 Trane<span className="text-primary">Hub</span>
