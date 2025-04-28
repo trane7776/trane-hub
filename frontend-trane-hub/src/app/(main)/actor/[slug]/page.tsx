4e1c34aaa3ea6e97e8116cbd82e356e2e7863fc9
trane7776 <izbasarov.t@edu.rea.ru>
2025-04-28 03:18:18 +0300

    fix

--- frontend-trane-hub/src/app/(main)/actor/[slug]/page.tsx
+++ frontend-trane-hub/src/app/(main)/actor/[slug]/page.tsx
@@ -2,53 +2,21 @@ import { Catalog } from '@/components/ui/catalog-movies/Catalog';
 import { actorService } from '@/services/actor.service';
 import { movieService } from '@/services/movie.service';
 import { IPageSlugParam, TypeParamSlug } from '@/types/page-params.types';
-import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
-export const revalidate = 60;
-
-export async function generateStaticParams() {
-    const actors = await actorService.getAll();
-
-    const paths = actors.map((actor) => {
-        return {
-            params: {
-                slug: actor.slug,
-            },
-        };
-    });
-
-    return paths;
-}
-
-async function getMovies(params: TypeParamSlug) {
-    try {
-        const { data: actor } = await actorService.getBySlug(params.slug);
-        if (!actor) return redirect('/404');
-
-        const { data: movies } = await movieService.getByActor(actor.id);
-        return { actor, movies };
-    } catch (error) {
-        return redirect('/404');
-    }
-}
-
-export async function generateMetadata({
-    params,
-}: IPageSlugParam): Promise<Metadata> {
-    const { actor, movies } = await getMovies(params);
-
-    return {
-        title: actor.name,
-        openGraph: {
-            images: {
-                url: actor.photoUrl,
-            },
-        },
-    };
-}
-
 export default async function ActorPage({ params }: IPageSlugParam) {
+    async function getMovies(params: TypeParamSlug) {
+        try {
+            const { data: actor } = await actorService.getBySlug(params.slug);
+            if (!actor) return redirect('/404');
+
+            const { data: movies } = await movieService.getByActor(actor.id);
+            return { actor, movies };
+        } catch (error) {
+            return redirect('/404');
+        }
+    }
+
     const { actor, movies } = await getMovies(params);
 
     return (
--- frontend-trane-hub/src/app/(main)/genre/[slug]/page.tsx
+++ frontend-trane-hub/src/app/(main)/genre/[slug]/page.tsx
@@ -2,56 +2,21 @@ import { Catalog } from '@/components/ui/catalog-movies/Catalog';
 import { genreService } from '@/services/genre.service';
 import { movieService } from '@/services/movie.service';
 import { IPageSlugParam, TypeParamSlug } from '@/types/page-params.types';
-import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
-export const revalidate = 60;
-
-export async function generateStaticParams() {
-    const genres = await genreService.getAll();
-
-    const paths = genres.map((genre) => {
-        return {
-            params: {
-                slug: genre.slug,
-            },
-        };
-    });
-
-    return paths;
-}
-
-async function getMovies(params: TypeParamSlug) {
-    try {
-        const { data: genre } = await genreService.getBySlug(params.slug);
-        if (!genre) return redirect('/404');
-
-        const { data: movies } = await movieService.getByGenres([genre.id]);
-        return { genre, movies };
-    } catch (error) {
-        return redirect('/404');
-    }
-}
-
-export async function generateMetadata({
-    params,
-}: IPageSlugParam): Promise<Metadata> {
-    const { genre, movies } = await getMovies(params);
-
-    return {
-        title: genre.name,
-        description: genre.description,
-        openGraph: {
-            images: {
-                url: movies[0]?.poster,
-            },
-
-            description: genre.description,
-        },
-    };
-}
-
 export default async function GenrePage({ params }: IPageSlugParam) {
+    async function getMovies(params: TypeParamSlug) {
+        try {
+            const { data: genre } = await genreService.getBySlug(params.slug);
+            if (!genre) return redirect('/404');
+
+            const { data: movies } = await movieService.getByGenres([genre.id]);
+            return { genre, movies };
+        } catch (error) {
+            return redirect('/404');
+        }
+    }
+
     const { genre, movies } = await getMovies(params);
 
     return (
